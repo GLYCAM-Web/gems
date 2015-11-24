7ce651647063e9efea0937000f338f2bdef3cc30
Lachele Foley <lachele@gmail.com>
2015-11-24 01:26:58 -0500

    making make.sh a little easier and fixing a puzzing bug in bin/detect_sugars

--- make.sh
+++ make.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
 MOOD="$1"
 TARGET_MAKE_FILE="Makefile-main"
 echo $TARGET_MAKE_FILE
 echo $MOOD
+if [ -z "$PYTHON_HOME" ] ; then
+	echo "PYTHON_HOME is not set.  Attempting to set it to /usr/include/python2.7 ..."
+	if [ -e "/usr/include/python2.7/Python.h" ] ; then 
+		export PYTHON_HOME="/usr/include/python2.7"
+		echo "PYTHON_HOME has been set to /usr/include/python2.7."
+	else
+		echo "Cannot automatically set PYTHON_HOME environment variable.  Exiting."
+		echo "Your PYTHON_HOME should be set to the location of Python.h for your python2.7 installation."
+		exit 1
+	fi
+fi
+
 cd gmml
 if [ -f $TARGET_MAKE_FILE ]; then
 	if [ "$MOOD" == "Qt" ]; then
