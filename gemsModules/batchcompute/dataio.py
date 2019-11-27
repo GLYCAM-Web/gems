da75aa8705df58aa3074d6bf01a5a8153a2eec01
Lachele Foley <lachele@gmail.com>
2019-11-26 19:29:24 -0500

    committing to go test something.

--- gemsModules/batchcompute/dataio.py
+++ gemsModules/batchcompute/dataio.py
@@ -6,3 +6,32 @@ from pydantic import BaseModel, Schema
 from pydantic.schema import schema
 
 
+##
+##  Job options....
+##  
+##  name
+##  working directory
+##  queue/partition
+##  time limit
+##  memory limit
+##  priority
+##  
+
+##  Scheduler Info....
+##
+##  type of scheduler (slurm, torque, ll, NONE, etc.) - NONE means use localhost and just run the job
+##  location of scheduler (host name , port)
+##  
+
+class SubmissionSchema(BaseModel):
+    schedulerInfo : 
+    jobOptions : None
+    options : Tags = None
+
+
+def generateSchema():
+    import json
+    print(SubmissionSchema.schema_json(indent=2))
+
+if __name__ == "__main__":
+  generateSchema()
