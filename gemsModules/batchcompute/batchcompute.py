ec7c15920bc968032d2766d6effccb4e65bb2e4a
Yao <yx55537@gmail.com>
2020-01-18 01:42:48 -0500

    Added batchcompute delegation framework. Build Amber-batchcompute-gRPC-slurm pipeline

--- gemsModules/batchcompute/batchcompute.py
+++ gemsModules/batchcompute/batchcompute.py
@@ -13,20 +13,8 @@ def batch_compute_delegation (incoming_json_dict):
     sbatch_argument = incoming_json_dict["sbatchArgument"]
     
     minimal_json_dict = incoming_json_dict
-    with open(workdir + "/" + "basic_submission.json", "w") as slurm_basic_json: 
-        json.dump(minimal_json_dict, slurm_basic_json)         
-
-    json_path = workdir + "/" + "basic_submission.json"
-    print ("/programs/gems/bin/slurmreceive " + json_path)
-    try:
-        p = subprocess.Popen( [ "/programs/gems/bin/slurmreceive ", json_path] ,
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        (out, err) = p.communicate()
-        print ("Stdout is:" + str(out) + "\n")
-        print ("Stderr is:" + str(err) + "\n")
-    except Exception as error:
-        print("Was unable to call /programs/gems/bin/slurmreceive\n")
-        print(error.message)
+    output_str = json.dumps(minimal_json_dict)
+    slurm_receive.manageIncomingString(output_str)
 
     
 if (__name__ == '__main__'):
