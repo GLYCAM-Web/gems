46818792436df527a7bf8a0a8754b47213c9838d
dan <danwentworthart@gmail.com>
2021-03-11 14:43:07 -0500

    Distinguishing between evaluate and preprocessPdbForAmber service requests.

--- gemsModules/structureFile/receive.py
+++ gemsModules/structureFile/receive.py
@@ -4,7 +4,7 @@ from gemsModules.common.services import *
 from gemsModules.common.transaction import *
 from gemsModules.project.projectUtil import *
 from gemsModules.common.loggingConfig import *
-from gemsModules.structureFile.amber.receive import preprocessPdbForAmber
+from gemsModules.structureFile.amber.receive import preprocessPdbForAmber, evaluatePdb
 import gemsModules.structureFile.settings as structureFileSettings
 import traceback
 
@@ -21,11 +21,14 @@ def receive(thisTransaction):
     #Look to see if services are specified, else do default.
     if 'services' not in thisTransaction.request_dict['entity'].keys():
         doDefaultService(thisTransaction)
+        
     else:
         services = getTypesFromList(thisTransaction.request_dict['entity']['services'])
         log.debug("requestedServices: " + str(services))
+
         for requestedService in services:
             log.debug("requestedService: " + str(requestedService))
+
             if requestedService not in structureFileSettings.serviceModules.keys():
                 log.error("The requested service is not recognized.")
                 log.error("services: " + str(structureFileSettings.serviceModules.keys()))
@@ -33,19 +36,29 @@ def receive(thisTransaction):
                 raise AttributeError(requestedService)
             elif requestedService == "PreprocessPdbForAmber":
                 try:
+                    log.debug("This is still in development.")
                     preprocessPdbForAmber(thisTransaction)
                 except Exception as error:
                     log.error("There was a problem preprocessing the PDB for amber: " + str(error))
+                    log.error(traceback.format_exc())
                     raise error
-                else:
-                    thisTransaction.build_outgoing_string()
+                
+                thisTransaction.build_outgoing_string()
+
+            elif requestedService == "Evaluate":
+                try:
+                    evaluatePdb(thisTransaction)
+                except Exception as error:
+                    log.error("There was a problem evaluating the pdb: " + str(error))
+                    log.error(traceback.format_exc())
+                    raise error
 
 def doDefaultService(thisTransaction):
     log.info("doDefaultService() was called.\n")
     ##Preprocess PDB will be the default. Given a request to the StructureFile entity,
     ##  with no services or options defined, look for a pdb file and preprocess it for Amber.
     try:
-        preprocessPdbForAmber(thisTransaction)
+        evaluatePdb(thisTransaction)
     except Exception as error:
         log.error("There was a problem doing the default service in the structureFile module.")
         raise error
