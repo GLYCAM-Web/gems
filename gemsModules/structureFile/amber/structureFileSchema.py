ad75a6ed8a9504c6f7fd7b463954398783015eed
Dan <danwentworthart@gmail.com>
2021-08-20 13:30:45 -0400

    Simple script allows schema generation without mucking with io main()
    In most io.py files, a main method allows one to print out the schemas for that file. In order to bypass that, I created a script that can be called separately to generate the schema for the website, placing them in a mounted dir it can find.

--- gemsModules/structureFile/amber/structureFileSchema.py
+++ gemsModules/structureFile/amber/structureFileSchema.py
@@ -1,42 +1,5 @@
 #!/usr/bin/env python3
-import os, sys, json, importlib.util
-import pathlib
-import traceback
 import gemsModules.structureFile.amber.io as amberIO
-from gemsModules.common.loggingConfig import *
-from gemsModules.common.logic import getGemsHome
-
-# from gemsModules.common.logic import updateResponse, getGemsHome
-
-
-if loggers.get(__name__):
-    pass
-else:
-    log = createLogger(__name__)
-
-# 1) Define one structure for each type of record.
-## UnrecognizedAtom ##
-## UnrecognizedMolecule ##
-## MissingResidue ##
-## HistidineProtonation ##
-## DisulfideBond ##
-## ChainTermination ##
-## ReplacedHydrogen ##
-
-# 2) Provide table metadata for things like labels.
-
-##  @brief Build the data needed for the pdb options tables.
-##  @detail Use classes in io.py to generate schema that will be used to build models.py. 
-##          Primarily for use by the pdb app in the website.
-def generateStructureFileSchemaForWeb():
-    log.info("generateStructureFileSchemaForWeb() was called.")
-    try:
-        amberIO.generateSchemaForWeb()
-    except Exception as error:
-        log.error("There was a problem generating the schema for the structureFile gemsModule: " + str(error))
-        log.error(traceback.format_exc())
-        raise error
-
 
 if __name__ == "__main__":
-    generateStructureFileSchemaForWeb()
+    amberIO.generateSchemaForWeb()
