7afe83adeb67536d9c002e1c5bc5059a56493a8c
Dan <danwentworthart@gmail.com>
2021-09-27 15:19:30 -0400

    Turned off preprocessPdbForAmber temporarily.
    So I can focus on getting evaluate working again after moving logic.py to refs.

--- gemsModules/structureFile/amber/preprocess.py
+++ gemsModules/structureFile/amber/preprocess.py
@@ -8,15 +8,24 @@ else:
     log = createLogger(__name__)
 
 
-def preprocessPdbForAmber(receivedTransaction : amberIO.Transaction):
+def preprocessPdbForAmber(receivedTransaction : amberIO.PdbTransaction):
     log.info("preprocessPdbForAmber() was called. Still in Development!!!!!!!!")
-    output = amberIO.PreprocessPdbForAmberOutput()
+
+    # ## Grab the input
+    # try:
+    #     uploadFile = getPdbFileFromTransaction(receivedTransaction)
+    # except Exception as error:
+    #     log.error("There was a problem getting the pdbFile from the transaction: " + str(error))
+    #     log.error(traceback.format_exc())
+    #     raise error
+
+
 
     ##TODO: write the logic to evaluate, and then write the processed pdb out to file.
 
     ## Keep this stuff for a reference, but replace it with better stuff. 
     ##VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-    
+    #output = amberIO.PreprocessPdbForAmberOutput()
     # try:
     #     pdbFile = generatePdbFile(receivedTransaction)
     #     log.debug("pdbFile output: " + str(pdbFile))
