439ded34033c65f84b9d7ec99ec20d1f8395b0ed
Charles Exum <charlesexumdev@gmail.com>
2021-10-05 16:19:05 -0400

    refactored logical errors in tests/009.test-disabled.Delegate_Invalid_Inputs.sh

--- tests/009.test-disabled.Delegate_Invalid_Inputs.sh
+++ tests/009.test-disabled.Delegate_Invalid_Inputs.sh
@@ -6,66 +6,67 @@
 is_Valid_JSON_py=$GEMSHOME/testbin/isvalidjson.py
 
 # json inputs
-bad_Payload=$GEMSHOME/gemsModules/delegator/test_in/sequence/minimal_bad_payload.json
+invalid_Payload=$GEMSHOME/gemsModules/delegator/test_in/sequence/minimal_invalid_payload.json
 invalid_Json=$GEMSHOME/gemsModules/delegator/test_in/sequence/invalid_json.json
 
-# invoke delegator script with a malformed .json
-# response should be a valid json object
-# with an error message
-run_Bad_Json_Input_Response_Test()
+# invoke delegator script with an invalid json object string
+# response should be a valid json object with an error message
+run_Invalid_Json_Input_Response_Test()
 {
 	# pipe .json to the delegator script
 	# pipe reponse from delegator to grep
 	# grep for "brief" : "NotAJSONObject"
 	# grep is returning a count (-c flag)
-	if [ $(cat $bad_Payload | $GEMSHOME/bin/delegate | grep -c '"brief" : NotAJSONObject') != '0' ] ; then
-		#echo "009a -- sequenceIsValid is True with known bad payload"
+	if [ $(cat $invalid_Json | $GEMSHOME/bin/delegate | grep -c '"brief" : NotAJSONObject') != '0' ] ; then
 		return 1
 	fi
 
-	if [ $(cat $bad_Payload | $GEMSHOME/bin/delegate | grep -c '"brief" : "NotAJSONObject"') != '0' ] ; then
-		#echo "009a -- sequenceIsValid is True with known bad payload"
+	if [ $(cat $invalid_Json | $GEMSHOME/bin/delegate | grep -c '"brief" : "NotAJSONObject"') != '0' ] ; then
 		return 1
 	fi
 	
 	return 0
 }
 
-# invoke delegator script with a .json containing a known bad payload
-# gmml should detect the bad payload and return a valid json object 
-# with an error message
-run_Valid_Json_Response_Test()
+# invoke delegator script with a json string containing a known invalid payload
+# gmml should detect the invalid payload and abort
+# gems should return a valid json object with an error message
+run_Invalid_Payload_Response_Test()
 {
 	# pipe .json to the delegator script, redirect to file f
 	# invoke json validator script 
-	cat $bad_Payload | $GEMSHOME/bin/delegate > f
+	cat $invalid_Payload | $GEMSHOME/bin/delegate > f
 	$is_Valid_JSON_py f
 	# exit code from is_Valid_JSON_py
 	isValid=$?
 	rm f
-	
+
 	if [ $isValid  != '0' ] ; then
 		return 1
 	fi
-
+	
+	## TODO: check for a specific error message
+	## currently this test is a false positive
 	return 0
 }
 
+# test to ensure that gmml doesn't attempt to build /w invalid payload
+# issue is described in github ticket #41
 # invoke delegator script with a .json containing a known bad payload
 # gmml should detect the bad payload and 
-# return a valid json response containing: "sequenceIsValid": "false"
-run_Bad_Payload_Test()
+# return a valid json response containing: "sequenceIsValid": "false"#  
+run_Invalid_Payload_Test()
 {
 	# pipe .json to the delegator script
 	# pipe reponse from delegator to grep
 	# grep for sequenceIsValid : true
 	# grep is returning a count (-c flag)
-	if [ $(cat $bad_Payload | $GEMSHOME/bin/delegate | grep -c '"sequenceIsValid": true') != '0' ] ; then
+	if [ $(cat $invalid_Payload | $GEMSHOME/bin/delegate | grep -c '"sequenceIsValid": true') != '0' ] ; then
 		#echo "009a -- sequenceIsValid is True with known bad payload"
 		return 1
 	fi
 
-	if [ $(cat $bad_Payload | $GEMSHOME/bin/delegate | grep -c '"sequenceIsValid": "true"') != '0' ] ; then
+	if [ $(cat $invalid_Payload | $GEMSHOME/bin/delegate | grep -c '"sequenceIsValid": "true"') != '0' ] ; then
 		#echo "009a -- sequenceIsValid is True with known bad payload"
 		return 1
 	fi
@@ -73,23 +74,15 @@ run_Bad_Payload_Test()
 	return 0
 }
 
-# invoke delegator script with a .json containing a known bad payload
-# gmml should detect the bad payload and 
-# return a valid json response containing an error message
-# run_Bad_Payload_Response_Test() 
-#{
-#	# write me
-#}
+# if ! run_Bad_Payload_Test; then
+# 	echo "009a -- sequenceIsValid is True with known bad payload"
+# 	echo "009a -- exiting"
+# 	exit 1
+# else
+# 	echo "009a -- passed"
+# fi
 
-if ! run_Bad_Payload_Test; then
-	echo "009a -- sequenceIsValid is True with known bad payload"
-	echo "009a -- exiting"
-	exit 1
-else
-	echo "009a -- passed"
-fi
-
-if ! run_Valid_Json_Response_Test; then
+if ! run_Invalid_Payload_Response_Test; then
 	echo "009b -- delegator returned an error as a non-valid json response"
 	echo "009b -- exiting"
 	exit 1
@@ -97,7 +90,7 @@ else
 	echo "009b -- passed"
 fi
 
-if ! run_Bad_Json_Input_Response_Test; then
+if run_Invalid_Json_Input_Response_Test; then
 	echo "009c -- grep did not find: brief : NotAJSONObject in response"
 	echo "009c -- exiting"
 	exit 1
