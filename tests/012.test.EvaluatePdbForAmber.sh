b8b1d80eb334720b5bc62b07b764ff422e774c0d
Dan <danwentworthart@gmail.com>
2021-12-09 16:53:37 -0500

    Test 12 bugfix
    plays nice with sh now.

--- tests/012.test.EvaluatePdbForAmber.sh
+++ tests/012.test.EvaluatePdbForAmber.sh
@@ -22,42 +22,42 @@ runTest()
         exit 1
     fi
 
-    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c '/website/TESTS/pdb/test_in/1BUY_modified.pdb') == '0' ] ; then
+    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c '/website/TESTS/pdb/test_in/1BUY_modified.pdb') = '0' ] ; then
         echo "Test 012 failed: Response failed to echo the expected PDB input file."
         exit 1
     fi
 
-    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c 'unrecognizedAtoms') == '0' ] ; then
+    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c 'unrecognizedAtoms') = '0' ] ; then
         echo "Test 012 failed: Response failed to list unrecognizedAtoms." 
         exit 1
     fi
 
-    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c "unrecognizedMolecules") == '0' ] ; then
+    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c "unrecognizedMolecules") = '0' ] ; then
         echo "Test 012 failed: Response failed to list unrecognizedMolecules."
         exit 1
     fi
 
-    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c 'missingResidues') == '0' ] ; then
+    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c 'missingResidues') = '0' ] ; then
         echo "Test 012 failed: Response failed to list missingResidues." 
         exit 1
     fi
     
-    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c 'histidineProtonations') == '0' ] ; then
+    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c 'histidineProtonations') = '0' ] ; then
         echo "Test 012 failed: Response failed to list histidineProtonations." 
         exit 1
     fi
 
-    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c 'disulfideBonds') == '0' ] ; then
+    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c 'disulfideBonds') = '0' ] ; then
         echo "Test 012 failed: Response failed to list disulfideBonds." 
         exit 1
     fi
     
-    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c 'chainTerminations') == '0' ] ; then
+    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c 'chainTerminations') = '0' ] ; then
         echo "Test 012 failed: Response failed to list chainTerminations." 
         exit 1
     fi
 
-    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c 'replacedHydrogens') == '0' ] ; then
+    if [ $(cat $jsonFile | $GEMSHOME/bin/delegate | grep -c 'replacedHydrogens') = '0' ] ; then
         echo "Test 012 failed: Response failed to list replacedHydrogens." 
         exit 1
     fi
