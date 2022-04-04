eeb1c2b5686ee79eedcc5178899f9c5d61f095a2
Charles Exum <charlesexumdev@gmail.com>
2022-04-04 14:12:56 -0400

    removed blanket import statement from mmservice/settings.py

--- gemsModules/mmservice/settings.py
+++ gemsModules/mmservice/settings.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from gemsModules import common
-from gemsModules.common.transaction import *
+
 from typing import Dict, List, Optional, Sequence, Set, Tuple, Union
 from pydantic import BaseModel
 
