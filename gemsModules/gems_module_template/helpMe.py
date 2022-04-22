525ba76c81702859fb6d7593409ccba3e9e7f140
Dan Wentworth <danwentworthart@gmail.com>
2022-04-22 14:27:12 -0400

    Edits the helpMe.py text to be generic.

--- gemsModules/gems_module_template/helpMe.py
+++ gemsModules/gems_module_template/helpMe.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 usageText="""
-this is a usage statement for a Structure File
+this is a usage statement for a new gems module
 """
 basicHelpText="""
-this is basic help for a Structure File
+this is basic help for a new gems module
 """
 moreHelpText="""
-this is more help for a MM Structure File
+this is more help for a new gems module
 """
