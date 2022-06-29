42155562a378926d117f61da39ea33d5dac3a675
B. Lachele Foley <lachele@gmail.com>
2022-06-29 02:07:03 -0400

    changing transaction_in/out to inputs and outputs

--- gemsModules/common/common_api.py
+++ gemsModules/common/common_api.py
@@ -36,8 +36,8 @@ class Transaction:
     initialization of this class is usually the domain of delegator.
     """
 #    incoming_string: str = None
-#    transaction_in: CommonAPI = None
-#    transaction_out: CommonAPI = None
+#    inputs: CommonAPI = None
+#    outputs: CommonAPI = None
 #    outgoing_string: str = None
 
     def get_API_type(self):
@@ -56,9 +56,9 @@ class Transaction:
                 log.error("incoming string was empty")
                 self.generate_error_response(Brief='InvalidInput')
                 return 1
-            self.populate_transaction_in(self.incoming_string, no_check_fields)
+            self.populate_inputs(self.incoming_string, no_check_fields)
             if initialize_out :
-                self.initialize_transaction_out_from_transaction_in()
+                self.initialize_outputs_from_inputs()
             return 0
         except Exception as error:
             errMsg = "problem with call to parse_raw() while instantiating transaction with: " + str(in_string)
@@ -67,34 +67,34 @@ class Transaction:
             self.generate_error_response(Brief='JsonParseEror',AdditionalInfo={'error': str(error)})
             return 1
 
-    def initialize_transaction_out_from_transaction_in(self):
-        log.info("initialize_transaction_out_from_transaction_in was called")
-        self.transaction_out = self.get_API_type() 
-        self.transaction_out = self.transaction_in.copy(deep=True)
-        log.debug("The transaction_out is: ")
-        log.debug(self.transaction_out.json(indent=2))
+    def initialize_outputs_from_inputs(self):
+        log.info("initialize_outputs_from_inputs was called")
+        self.outputs = self.get_API_type() 
+        self.outputs = self.inputs.copy(deep=True)
+        log.debug("The outputs is: ")
+        log.debug(self.outputs.json(indent=2))
 
     @validator('*',check_fields=False)
-    def populate_transaction_in_no_check_fields(self, in_string : str):
-        self.transaction_in = self.get_API_type().parse_raw(in_string)
+    def populate_inputs_no_check_fields(self, in_string : str):
+        self.inputs = self.get_API_type().parse_raw(in_string)
 
-    def populate_transaction_in(self, 
+    def populate_inputs(self, 
         in_string : str, 
         no_check_fields=False):
         if in_string is None:
-            raise Exception("in_string was None for populate_transaction_in")
+            raise Exception("in_string was None for populate_inputs")
         if no_check_fields:
-            self.populate_transaction_in_no_check_fields(in_string)
+            self.populate_inputs_no_check_fields(in_string)
         else: 
-            self.transaction_in = self.get_API_type().parse_raw(in_string)
-        log.debug("The transaction_in is: ")
-        log.debug(self.transaction_in.json(indent=2))
+            self.inputs = self.get_API_type().parse_raw(in_string)
+        log.debug("The inputs is: ")
+        log.debug(self.inputs.json(indent=2))
 
     def process(self) :
-        if self.transaction_in.entity.services.__root__ is None  or self.transaction_in.entity.services.__root__ == [] :
+        if self.inputs.entity.services.__root__ is None  or self.inputs.entity.services.__root__ == [] :
             self.doDefaultService()
         else :
-            self.transaction_in.entity.services.process()
+            self.inputs.entity.services.process()
 
     def doDefaultService(self) :
         self.marco()
@@ -103,61 +103,61 @@ class Transaction:
         return 'common'
 
     def marco(self) :
-        if self.transaction_out is None :
-            self.initialize_transaction_out_from_transaction_in()
-        self.transaction_out.entity.responses = Responses()
-        thisEntity = self.transaction_out.entity.entityType
-        if self.transaction_out.entity.services is None or not self.transaction_out.entity.services.is_present('Marco') :
-            self.transaction_out.entity.services.add_service(typename='Marco')
-        self.transaction_out.entity.responses.add_response(
+        if self.outputs is None :
+            self.initialize_outputs_from_inputs()
+        self.outputs.entity.responses = Responses()
+        thisEntity = self.outputs.entity.entityType
+        if self.outputs.entity.services is None or not self.outputs.entity.services.is_present('Marco') :
+            self.outputs.entity.services.add_service(typename='Marco')
+        self.outputs.entity.responses.add_response(
                 typename = thisEntity,
                 outputs = {'payload': 'Polo'})
         self.build_outgoing_string()
 
     def generate_error_response(self, Brief='UnknownError', EntityType=settings.WhoIAm, AdditionalInfo=None) :
-        self.transaction_out = CommonAPI.construct(entity=Entity.construct(entityType=EntityType))
-        self.transaction_out.notices.addDefaultNotice(Brief=Brief, Messenger=EntityType, AdditionalInfo=AdditionalInfo)
+        self.outputs = CommonAPI.construct(entity=Entity.construct(entityType=EntityType))
+        self.outputs.notices.addDefaultNotice(Brief=Brief, Messenger=EntityType, AdditionalInfo=AdditionalInfo)
         self.build_outgoing_string()
 
     def getProjectIn(self):
         log.info("getProjectFromTransactionIn() was called.\n")
         try:
             if all(v is not None for v in [
-                    self.transaction_in,
-                    self.transaction_in.project]):
-                log.debug("Found a non-None project in transaction_in of type : " +
-                          str(type(self.transaction_in.project)))
-                return self.transaction_in.project
+                    self.inputs,
+                    self.inputs.project]):
+                log.debug("Found a non-None project in inputs of type : " +
+                          str(type(self.inputs.project)))
+                return self.inputs.project
             else:
                 return None
         except Exception as error:
             log.error(
-                "There was a problem getting the project from transaction_in :  " + str(error))
+                "There was a problem getting the project from inputs :  " + str(error))
             raise error
 
     def getProjectOut(self):
         log.info("getProjectFromTransactionOut() was called.\n")
         try:
             if all(v is not None for v in [
-                    self.transaction_out,
-                    self.transaction_out.project]):
-                log.debug("Found a non-None project in transaction_out of type : " +
-                          str(type(self.transaction_out.project)))
-                return self.transaction_out.project
+                    self.outputs,
+                    self.outputs.project]):
+                log.debug("Found a non-None project in outputs of type : " +
+                          str(type(self.outputs.project)))
+                return self.outputs.project
             else:
                 return None
         except Exception as error:
             log.error(
-                "There was a problem getting the project from transaction_out :  " + str(error))
+                "There was a problem getting the project from outputs :  " + str(error))
             raise error
 
     def build_outgoing_string(self, prettyPrint=False, indent=2, prune_empty_values=True) :
-        if self.transaction_out.prettyPrint is True:
+        if self.outputs.prettyPrint is True:
             prettyPrint = True
         if prettyPrint :
-            self.outgoing_string = self.transaction_out.json(indent=2,exclude_none=prune_empty_values)
+            self.outgoing_string = self.outputs.json(indent=2,exclude_none=prune_empty_values)
         else:
-            self.outgoing_string = self.transaction_out.json(exclude_none=prune_empty_values)
+            self.outgoing_string = self.outputs.json(exclude_none=prune_empty_values)
 
     def get_outgoing_string(self):
         if self.outgoing_string is None or self.outgoing_string == "" :
