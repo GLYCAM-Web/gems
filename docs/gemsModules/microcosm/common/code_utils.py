b79a05f3f4acd91b98f913cf43159b183ee26a07
B. Lachele Foley <lachele@gmail.com>
2022-10-10 03:05:25 -0400

    Code design for services tree mostly settled

--- docs/gemsModules/microcosm/common/code_utils.py
+++ docs/gemsModules/microcosm/common/code_utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from enum import Enum
+from typing import  List
 
 from .logger import Set_Up_Logging
 Set_Up_Logging(__name__)
@@ -43,4 +44,36 @@ class GemsStrEnum(str, Enum):
             theList.append(item.name)
         return theList
 
+# The purpose of this class is to provide list metadata so that the
+# list can be ordered or unordered.  The default is ordered.
+# An unordered list might contain items that are executable and can
+# be executed in any order, or in parallel, etc.
+# If they are set as fire-and-forget, then the order is not important,
+# but they should also be executed in separate threads.
+class Annotated_List() :
+    def __init__(self,
+            items : List = [],
+            ordered  : bool   = True,
+            fire_and_forget : bool = False
+            ) -> None :
+        self.items : List = items
+        self.ordered  : bool = ordered
+        self.fire_and_forget : bool = fire_and_forget
 
+    def add_item(self, item) :
+        self.items.append(item)
+
+    def get_items(self) :
+        return self.items
+
+    def get_ordered(self) :
+        return self.ordered
+
+    def set_ordered(self, ordered) :   
+        self.ordered = ordered
+
+    def get_fire_and_forget(self) :
+        return self.fire_and_forget
+
+    def set_fire_and_forget(self, fire_and_forget) :   
+        self.fire_and_forget = fire_and_forget
