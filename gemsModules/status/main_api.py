e455d5e27fe3e7c8708e427b984952dc01eb686d
charles <charlesexum@gmail.com>
2023-02-14 14:25:58 -0500

    partial commit

--- gemsModules/status/main_api.py
+++ gemsModules/status/main_api.py
@@ -5,12 +5,19 @@ from gemsModules.common import main_api
 from gemsModules.common import main_api_entity
 
 ##
-#@detail
-# Holds information about the main object responsible for a service.
+#@detail write me
 class Status_Entity(main_api_entity.Entity):
 
     def getEntityType(self):
         return WhoIAm
 
+##
+#@detail write me
 class Status_API(main_api.Common_API):    
     entity : Status_Entity
+
+##
+#@detail write me
+class Status_Transaction(main_api.Transaction):
+    def get_API_type(self):
+        return Status_API
