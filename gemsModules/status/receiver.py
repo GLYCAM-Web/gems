664b1ff46f8ba522efae50a588f55e85d060bf5f
charles <charlesexum@gmail.com>
2023-02-20 14:54:24 -0500

    added boiler plate to main_api.py and receiver.py

--- gemsModules/status/receiver.py
+++ gemsModules/status/receiver.py
@@ -6,11 +6,20 @@ from gemsModules.status.main_api import Status_Transaction
 from gemsModules.logging.logger import Set_Up_Logging
 log = Set_Up_Logging(__name__)
 
+##
+#@detail 
+# write me
 class Status_Receiver(Receiver):
 
+    ##
+    #@detail 
+    # write me
     def get_incoming_entity_type(self) -> str:
         return WhoIAm
     
+    ##
+    #@detail 
+    # write me
     def get_transaction_child_type(self):
         return Status_Transaction
         
