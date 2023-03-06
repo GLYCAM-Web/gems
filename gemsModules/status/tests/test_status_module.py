0f217793270cb2818343dd8b12cecee01883a858
charles <charlesexum@gmail.com>
2023-03-06 09:51:16 -0500

    added comments to tests/test_status_module.py

--- gemsModules/status/tests/test_status_module.py
+++ gemsModules/status/tests/test_status_module.py
@@ -2,18 +2,30 @@ import unittest
 import json
 from receive import receive
 
-default_in = '{ "entity" : { "type": "Status" } }'
+_default_in = '{ "entity" : { "type": "Status" } }'
 
-def get_string_from_receive(input):
+##
+#@detail
+# write me
+def _get_string_from_receive(input: str) -> str:
     return receive(input)
 
+##
+#@detail
+# write me
 class TestStatusModule(unittest.TestCase):
-    
+    ##
+    #@brief
+    # assert receive() returns a str
     def test_receive_returns_string(self):
-        assert isinstance(get_string_from_receive(default_in), str)
+        assert isinstance(_get_string_from_receive(_default_in), str)
     
-    def test_receive_returns_json_formatted_string(self):    
-        assert isinstance(json.loads(default_in), object) #prove default_in is valud .json
-        assert isinstance(json.loads(get_string_from_receive(default_in)), object)
+    ##
+    #@detail
+    # assert receive() default_in is .json formatted str
+    # assert receive() returns a .json formatted str
+    def test_receive_returns_json_formatted_string(self):        
+        assert isinstance(json.loads(_default_in), object) #prove default_in is valud .json
+        assert isinstance(json.loads(_get_string_from_receive(_default_in)), object)
         
         
