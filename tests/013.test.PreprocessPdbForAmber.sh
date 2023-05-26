ce0e09ac75a72c54cfb38455c8a0dca094059db4
Oliver <olgrant@uga.edu>
2023-05-26 13:39:45 +0200

    Old preprocessor works again with changes in gmml.
    I tried to fix the old one first, made some progress and kept it. This will get replaced by a new one, but keeping it for now for comparison. Plus Dan is using it.

--- tests/013.test.PreprocessPdbForAmber.sh
+++ tests/013.test.PreprocessPdbForAmber.sh
@@ -1,82 +1,56 @@
-#!/bin/sh
+#!/bin/bash
 
 # Tests the ability to preprocess 1BUY 
 # Compare the preprocessing output to known valid output.
 
-## TODO: think of more meaningful tests. 
-##	1BUY is known to give quick results, but warning and error-level
-##	options will not be tested. Also not tested here is
-##	the ability to accept user-set options.
-
-echo "
-013: Testing pdb preprocessor output.
-"
+echo "013: Testing pdb preprocessor output."
 
 runTest()
 {
 	# JSON INPUTS
     jsonInputFile=$GEMSHOME/gemsModules/deprecated/delegator/test_in/pdb/preprocessPdbForAmber_file.json
-    if [ -f "$jsonInputFile" ]; then 
-        echo "Found test input: $jsonInputFile"
-    else
+    if [ ! -f "$jsonInputFile" ]; then 
         echo "Test 013 failed: Failed to find request input: $jsonInputFile"
         return 1
     fi
-
-
+    echo "Found test input: $jsonInputFile"
     ## Correct outputs ref
     outputRefFile=${GEMSHOME}/tests/correct_outputs/test013_output.pdb
-    if [ -f "$outputRefFile" ]; then 
-        echo "Found correct_outputs reference: $outputRefFile"
-    else
+    if [ ! -f "$outputRefFile" ]; then 
         echo "Test 013 failed: Failed to find the correct_outputs reference: $outputRefFile"
         return 1
     fi
-
     testResponse="${GEMSHOME}/tests/bad_outputs/${now}_git-ignore-me_test_response_013.json"
     echo "testResponse filename will be: $testResponse"
-
     cat $jsonInputFile | python $GEMSHOME/bin/delegate > $testResponse
     line=$( cat $testResponse| grep "project_dir" )
     #echo "line: $line"
     preface=$( echo $line | sed -n -e 's/^.*project_dir\": \"//p' )
     projectDir=$( echo $preface | sed -n -e 's/\",.*//p' )
     outputFile="${projectDir}/updated_pdb.pdb"
-
     # Updated pdb
- 
-    if [ -f "$outputFile" ]; then 
-        echo "Found outputFile:"
-        echo "$outputFile"
-        echo "comparing to outputRefFile:"
-        echo "$outputRefFile"
-    else
+    if [ ! -f "$outputFile" ]; then 
         echo "Test 013 failed: Failed to find the outputFile:"
         echo "$outputFile"
         return 1
     fi
-
-
-    if $( diff $outputFile  $outputRefFile ); then
-        echo "Test passed."
-        rm $outputFile
-        rm $testResponse
-    else
+    echo "Comparing $outputFile $outputRefFile"
+    if ! cmp -s $outputFile $outputRefFile; then
         now=$(date "+%Y-%m-%d-%H-%M-%S")
         errorFile="${GEMSHOME}/tests/bad_outputs/${now}_git-ignore-me_test_error_013.txt"
-        echo "
-Test failed: unexpected response
-Look for details in $errorFile
-"
-        echo "Test failed: unexpected response"
-        echo $( diff $outputFile  $outputRefFile ) > $errorFile
+        echo "Test failed: unexpected response. Look for details in $errorFile"
+        echo "Test failed, these files are different: $outputFile $outputRefFile"
+        echo $( diff $outputFile $outputRefFile ) > $errorFile
         return 1
     fi
-
+    echo "Test passed."
+    rm $outputFile
+    rm $testResponse
     return 0
 }
 
-if runTest; then
-	echo "013 -- passed"
-	return 0
+if ! runTest; then
+	return 1
 fi
+echo "013 -- passed"
+return 0
