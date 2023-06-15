cb4114812d109a891d2ae3b20f313acb8459a45a
Grayson Miller <grayson.miller124@gmail.com>
2023-06-15 18:09:04 +0000

    test script update and some readme stuff

--- tests/016.test.AmberMDPrep.sh
+++ tests/016.test.AmberMDPrep.sh
@@ -2,9 +2,9 @@
 THISPYTHON='python3'
 testNumber=016
 echo "Testing $0..."
-
 preprocess_file="/programs/gems/tests/outputs/016.AmberMDPrep.4mbzEdit-git-ignore-me.prepared.pdb"
 
+# clean up from previous runs
 if [ -f $preprocess_file ]; then
     rm "${preprocess_file}"
 fi
@@ -12,19 +12,37 @@ if [ -f test${testNumber}_output ]; then
     rm test${testNumber}_output
 fi
 
-# Note, json_ripper will throw up if you don't give it a valid JSON object, 
+# sanity check due to bs bug
+# echo "ENV CHECK:"
+# echo "$GEMSHOME"
+# echo "$(which ${THISPYTHON})"
+# pwd 
+
+if ! [ -f "${GEMSHOME}/gemsModules/ambermdprep/tests_in/explicit.json" ]; then
+    printf "Test FAILED! File %s does not exist \n" ${GEMSHOME}/gemsModules/ambermdprep/tests_in/explicit.json
+    return 1
+fi
+
+# Note, json_ripper will throw up if you don't give it a valid JSON object, (This is reason for the TypeError failure in the test run from prepush)
 # so the test will fail in that case too. jq would be more convenient...
-cat $GEMSHOME/gemsModules/ambermdprep/tests_in/explicit.json | $GEMSHOME/bin/delegate |\
+#
+# This should not fail, however old JSON_from_Command_line will fail if abberant stdin is given, such as via git prepush
+# ${GEMSHOME}/bin/delegate ${GEMSHOME}/gemsModules/ambermdprep/tests_in/explicit.json |\
+#
+# This works with prepush:
+cat ${GEMSHOME}/gemsModules/ambermdprep/tests_in/explicit.json | ${GEMSHOME}/bin/delegate |\
     $GEMSHOME/tests/utilities/json_ripper.py entity.responses.any_amber_prep.outputs.message > test${testNumber}_output
 
+# echo -e "\nRipped Output Message Bytes: $(wc -c < test${testNumber}_output) (Expected $(wc -c < correct_outputs/test${testNumber}_output))"
 if ! cmp test${testNumber}_output correct_outputs/test${testNumber}_output > /dev/null 2>&1; then
     printf "Test FAILED! Output file %s different from %s \n" test${testNumber}_output correct_outputs/test${testNumber}_output
     return 1
 fi
 if ! [ -f $preprocess_file ]; then
-    echo "Test FAILED! Did not create preprocessed.pdb"
+    echo "Test FAILED! Did not create $preprocess_file"
     return 1
 fi
+
 echo "Test passed."
 rm test${testNumber}_output $preprocess_file
 return 0
