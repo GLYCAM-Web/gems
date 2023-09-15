924e0f912cd033c4f1ed60e700b28e50824c2258
Grayson Miller <grayson.miller124@gmail.com>
2023-09-15 13:36:29 -0600

    unittest for instance config, some bugfixes for unused paths, updated example

--- gemsModules/systemoperations/instance_ops.py
+++ gemsModules/systemoperations/instance_ops.py
@@ -39,8 +39,9 @@ class InstanceConfig(dict):
         self.update(self.load())
 
     @staticmethod
-    def load() -> dict:
-        instance_config_path = Path(os.getenv("GEMSHOME", "")) / "instance_config.json"
+    def load(instance_config_path=None) -> dict:
+        if instance_config_path is None:
+            instance_config_path = InstanceConfig.get_default_path()
         if not instance_config_path.exists():
             raise FileNotFoundError(
                 f"instance_config.json not found at GEMSHOME: {instance_config_path}\n"
@@ -50,6 +51,14 @@ class InstanceConfig(dict):
         with open(instance_config_path, "r") as f:
             instance_config = json.load(f)
         return instance_config
+
+    @staticmethod
+    def get_default_path(example=False) -> Path:
+        name = "instance_config.json"
+        if example:
+            name += ".example"
+
+        return Path(os.getenv("GEMSHOME", "")) / name
 
     def get_available_contexts(self, instance_hostname=None) -> list:
         """
@@ -65,19 +74,22 @@ class InstanceConfig(dict):
         return available_contexts
 
     def get_possible_hosts_for_context(
-        self, context: str, with_slurmport=False
+        self, context: str, with_slurmport=False, return_names=False
     ) -> list:
         """
         Returns a list of possible hosts for a given context.
         """
         possible_hosts = []
-        for host in self["hosts"].values():
+        for name, host in self["hosts"].items():
             for host_context in host["contexts"]:
                 if host_context == context:
-                    if with_slurmport:
-                        possible_hosts.append(f"{host['host']}:{host['slurmport']}")
+                    if return_names:
+                        possible_hosts.append(name)
                     else:
-                        possible_hosts.append(host["host"])
+                        if with_slurmport:
+                            possible_hosts.append(f"{host['host']}:{host['slurmport']}")
+                        else:
+                            possible_hosts.append(host["host"])
         return possible_hosts
 
     # sbatch argument helpers
@@ -89,7 +101,11 @@ class InstanceConfig(dict):
         """Returns a list of possible sbatch arguments per host for a given context."""
         l = []
         for host in self["hosts"].values():
-            l.append(host["sbatch_arguments"])
+            if context in host["contexts"] and "sbatch_arguments" in host:
+                l.append(host["sbatch_arguments"])
+
+        l.append(self["default_sbatch_arguments"][context])
+        return l
 
     def get_named_hostname(self, name) -> str:
         """Returns the actual hostname of a named host.
@@ -105,6 +121,10 @@ class InstanceConfig(dict):
                 return name
         return None
 
+    def get_sbatch_arguments_by_named_host(self, name) -> dict[str, dict]:
+        """Returns a dict of possible sbatch arguments per context for a given named host."""
+        return self["hosts"][name]["sbatch_arguments"]
+
     def get_sbatch_arguments_by_hostname(self, hostname) -> dict[str, dict]:
         """Returns a dict of possible sbatch arguments per context for a given host."""
         name = self.get_name_by_hostname(hostname)
@@ -112,6 +132,7 @@ class InstanceConfig(dict):
 
     def get_sbatch_arguments(self, host=None, context=None):
         """Returns the sbatch arguments for a given host and context."""
+
         if context is None:
             # TODO: Is this sensible?
             if is_GEMS_test_workflow():
@@ -124,11 +145,13 @@ class InstanceConfig(dict):
             if context is None:
                 return sb_arg_dict
             else:
-                possible_hosts = self.get_possible_hosts_for_context(context)
+                possible_hosts = self.get_possible_hosts_for_context(
+                    context, return_names=True
+                )
                 # TODO: More complicated selection of proper host
                 host = possible_hosts.pop()
 
-        possible_sbatch_args = self.get_sbatch_arguments_by_hostname(host)
+        possible_sbatch_args = self.get_sbatch_arguments_by_named_host(host)
         for ctx, args in possible_sbatch_args.items():
             if ctx == context:
                 sb_arg_dict.update(args)
--- gemsModules/systemoperations/tests/test_InstanceConfig.py
+++ gemsModules/systemoperations/tests/test_InstanceConfig.py
@@ -1,4 +1,6 @@
+import shutil
 import unittest
+import pathlib, os
 from gemsModules.systemoperations.instance_ops import InstanceConfig
 
 
@@ -13,8 +15,91 @@ class Test_InstanceConfig(unittest.TestCase):
         Set up for testing InstanceConfig.
         """
         self.ic = InstanceConfig()
+        example_path = self.ic.get_default_path(example=True)
+        # copy example to cwd temporarily
+        self.test_ic_path = pathlib.Path(os.getcwd()) / example_path.name.replace(
+            ".example", ""
+        )
+        shutil.copyfile(example_path, self.test_ic_path)
+
+        self.ic.load(instance_config_path=self.test_ic_path)
+
+    def tearDown(self) -> None:
+        os.remove(self.test_ic_path)
+
+    def test_load(self):
+        """
+        Test that the load method correctly loads the JSON content.
+        """
+        config = self.ic.load(instance_config_path=self.test_ic_path)
+        self.assertIsInstance(config, dict)
+        self.assertIn("hosts", config)
+
+    def test_get_default_path(self):
+        """
+        Test that the get_default_path returns a valid path.
+        """
+        default_path = self.ic.get_default_path()
+        self.assertIsInstance(default_path, pathlib.Path)
 
     def test_get_available_contexts(self):
         """
         Test the get_available_contexts method.
         """
+        contexts = self.ic.get_available_contexts(instance_hostname="gw-grpc-delegator")
+        self.assertIsInstance(contexts, list)
+
+    def test_get_possible_hosts_for_context(self):
+        """
+        Test the get_possible_hosts_for_context method.
+        """
+        hosts = self.ic.get_possible_hosts_for_context(context="MdaaS-RunMD")
+        self.assertIsInstance(hosts, list)
+
+    def test_get_default_sbatch_arguments(self):
+        """
+        Test the get_default_sbatch_arguments method.
+        """
+        sbatch_args = self.ic.get_default_sbatch_arguments(context="Default")
+        self.assertIsInstance(sbatch_args, dict)
+
+    def test_get_sbatch_arguments_by_context(self):
+        """
+        Test the get_sbatch_arguments_by_context method.
+        """
+        sbatch_args = self.ic.get_sbatch_arguments_by_context(context="DevEnv")
+        self.assertIsInstance(sbatch_args, list)
+
+    def test_get_named_hostname(self):
+        """
+        Test the get_named_hostname method.
+        """
+        hostname = self.ic.get_named_hostname(name="swarm")
+        self.assertIsInstance(hostname, str)
+
+    def test_get_name_by_hostname(self):
+        """
+        Test the get_name_by_hostname method.
+        """
+        name = self.ic.get_name_by_hostname(hostname="harper")
+        self.assertIsInstance(name, str)
+
+    def test_get_sbatch_arguments_by_hostname(self):
+        """
+        Test the get_sbatch_arguments_by_hostname method.
+        """
+        sbatch_args = self.ic.get_sbatch_arguments_by_hostname(hostname="gw-slurm-head")
+        self.assertIsInstance(sbatch_args, dict)
+
+    def test_get_sbatch_arguments(self):
+        """
+        Test the get_sbatch_arguments method.
+        """
+        sbatch_args = self.ic.get_sbatch_arguments(
+            host="thoreau", context="MDaaS-RunMD"
+        )
+        self.assertIsInstance(sbatch_args, dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
