7c95e73ac80eec28f5c82d5df9b22b5d543715a2
Grayson Miller <grayson.miller124@gmail.com>
2023-09-19 12:18:19 -0600

    fixes for website tests: test_marco_polo and test_listEntities
    - forgot to return incoming string on delegator process when valid.
    - marco's service api was using serviceOutputs, which for the moment due to how the Entity BaseModel is set up, duplicates the outputs and nests it.

--- gemsModules/common/services/marco/logic.py
+++ gemsModules/common/services/marco/logic.py
@@ -4,49 +4,65 @@ from pydantic import BaseModel
 
 from gemsModules.common.tasks import cake
 
-from gemsModules.logging.logger import Set_Up_Logging 
+from gemsModules.logging.logger import Set_Up_Logging
+
 log = Set_Up_Logging(__name__)
 
+
 ## These are a little redundant in this simple example.
-class requestInputs(BaseModel):  
+class requestInputs(BaseModel):
     """Defines the inputs to the service."""
-    entity : str = None
-    who_I_am : str  = None
+
+    entity: str = None
+    who_I_am: str = None
+
 
 class responseOutputs(BaseModel):
     """Defines the outputs from the service."""
-    message : str = None
-    info : Optional[str] = None
 
-class serviceInputs (Protocol):
+    message: str = None
+    info: Optional[str] = None
+
+
+class serviceInputs(Protocol):
     """Allows a Service_Request to be used as input"""
-    inputs : requestInputs = requestInputs()
-    options : Dict[str,str]
+
+    inputs: requestInputs = requestInputs()
+    options: Dict[str, str]
+
 
 class serviceOutputs(BaseModel):
-    outputs : responseOutputs = responseOutputs()
+    outputs: responseOutputs = responseOutputs()
+
 
 class cakeInputs:
-    cake : bool = False
-    color : str = None
+    cake: bool = False
+    color: str = None
 
-def execute(inputs : serviceInputs) -> serviceOutputs:
+
+def execute(inputs: serviceInputs) -> responseOutputs:
     log.debug(f"serviceInputs: {serviceInputs}")
-    service_outputs = serviceOutputs()
+
+    # We're not using serviceOutputs because it doubles up .outputs
+    service_outputs = responseOutputs()
     # The who_I_am must be set in the options.
-    if inputs.inputs.entity == inputs.inputs.who_I_am :  # trivial here, but could be more complex (search a dictionary, etc.).
-        service_outputs.outputs.message="Polo"
-    else : 
-        service_outputs.outputs.message="Marco request sent to wrong entity. See who_I_am in info."
-    if inputs.options is not None :
+    if (
+        inputs.inputs.entity == inputs.inputs.who_I_am
+    ):  # trivial here, but could be more complex (search a dictionary, etc.).
+        service_outputs.message = "Polo"
+    else:
+        service_outputs.outputs.message = (
+            "Marco request sent to wrong entity. See who_I_am in info."
+        )
+    if inputs.options is not None:
         cake_inputs = cakeInputs()
-        docake=False
-        if 'cake' in inputs.options.keys() :
-            docake=True
-            cake_inputs.cake = inputs.options['cake']
-        if 'color' in inputs.options.keys() :
-            docake=True
-            cake_inputs.color = inputs.options['color']
-        if docake == True :
+        docake = False
+        if "cake" in inputs.options.keys():
+            docake = True
+            cake_inputs.cake = inputs.options["cake"]
+        if "color" in inputs.options.keys():
+            docake = True
+            cake_inputs.color = inputs.options["color"]
+        if docake == True:
             service_outputs.outputs.info = cake.execute(cake_inputs)
     return service_outputs
--- gemsModules/delegator/receive.py
+++ gemsModules/delegator/receive.py
@@ -3,22 +3,22 @@ from gemsModules.delegator.json_string_manager import Redirector_Json_String_Man
 from gemsModules.delegator.json_string_manager import Delegator_Json_String_Manager
 from gemsModules.delegator.main_settings import WhoIAm
 from gemsModules.logging.logger import Set_Up_Logging
+
 log = Set_Up_Logging(__name__)
+
 
 def process(incomingString: str) -> str:
     log.info("Delegator was called as an entity.  Processing.")
     delegator_string_manager = Delegator_Json_String_Manager()
-    delegator_string_manager_error_response = delegator_string_manager.process(incoming_string = incomingString)
+    delegator_string_manager_error_response = delegator_string_manager.process(
+        incoming_string=incomingString
+    )
     if delegator_string_manager_error_response is not None:
         log.debug("The incoming string is not valid")
         return delegator_string_manager_error_response
-    log.debug("The incoming string is valid")
-
-    ## Grab the incoming Entity
-
-    ## Hand the entity off to the service manager
-
-    ## Generate and return outgoing string
+    else:
+        log.debug("The incoming string is valid")
+        return delegator_string_manager.transaction.get_outgoing_string()
 
 
 def receive(incomingString: str) -> str:
@@ -26,7 +26,9 @@ def receive(incomingString: str) -> str:
 
     string_manager = Redirector_Json_String_Manager()
 
-    string_manager_error_response_string = string_manager.process(incoming_string = incomingString)
+    string_manager_error_response_string = string_manager.process(
+        incoming_string=incomingString
+    )
 
     if string_manager_error_response_string is not None:
         log.debug("The incoming string is not valid")
@@ -37,21 +39,24 @@ def receive(incomingString: str) -> str:
         return string_manager_error_response_string
 
     log.debug("The incoming string is valid")
-    requested_entity=string_manager.get_incoming_entity_type()
-########## 
-## NOTE!!!
-## this is temporary until Delegator's services work
-#        log.debug("Delegating incoming string to entity: " + requested_entity)
-#        from gemsModules.delegator import redirector_settings 
-#        entity_module = redirector_settings.Known_Entity_Reception_Modules[requested_entity]
-#        return entity_module(incomingString)
-##
-## This is how it should eventually work:
+    requested_entity = string_manager.get_incoming_entity_type()
+    ##########
+    ## NOTE!!!
+    ## this is temporary until Delegator's services work
+    #        log.debug("Delegating incoming string to entity: " + requested_entity)
+    #        from gemsModules.delegator import redirector_settings
+    #        entity_module = redirector_settings.Known_Entity_Reception_Modules[requested_entity]
+    #        return entity_module(incomingString)
+    ##
+    ## This is how it should eventually work:
     if requested_entity == WhoIAm:
         log.debug("Delegating incoming string to self")
         return process(incomingString)
     else:
         log.debug("Delegating incoming string to entity: " + requested_entity)
-        from gemsModules.delegator import redirector_settings 
-        entity_module = redirector_settings.Known_Entity_Reception_Modules[requested_entity]
+        from gemsModules.delegator import redirector_settings
+
+        entity_module = redirector_settings.Known_Entity_Reception_Modules[
+            requested_entity
+        ]
         return entity_module(incomingString)
