9c43814b17acc8d6ffef1f4ca01afb4629a50854
Grayson Miller <grayson.miller124@gmail.com>
2023-09-21 00:13:38 -0600

    on the way to fixing new delegator listEntities and Entity Model

--- gemsModules/delegator/services/known_entities/server.py
+++ gemsModules/delegator/services/known_entities/server.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
-from gemsModules.common.main_api_services import Service_Request,  Service_Response
+from gemsModules.common.main_api_services import Service_Request, Service_Response
+from gemsModules.common.main_api_notices import Notices
 
-from gemsModules.logging.logger import Set_Up_Logging 
+from gemsModules.delegator.tasks.get_known_entities import (
+    execute as get_known_entities,
+)
+from gemsModules.logging.logger import Set_Up_Logging
+
 log = Set_Up_Logging(__name__)
 
 
-def Serve(service : Service_Request) -> Service_Response:
-    pass
+def Serve(request: Service_Request) -> Service_Response:
+    log.info("The KnownEntities server was called.")
+    response = Service_Response()
 
+    response.typename = request.typename
+    response.outputs = {"message": get_known_entities()}
+    response.notices = Notices()
+
+    return response
--- gemsModules/delegator/services/settings/service_modules.py
+++ gemsModules/delegator/services/settings/service_modules.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 from typing import Dict, Callable
 from gemsModules.common.services.error.server import Serve as serve_error
-from gemsModules.common.services.list_services.server import Serve as serve_list_services
+from gemsModules.common.services.list_services.server import (
+    Serve as serve_list_services,
+)
 from gemsModules.common.services.marco.server import Serve as serve_marco
 from gemsModules.common.services.status.server import Serve as serve_status
 
-from gemsModules.delegator.services.known_entities.server import Serve as serve_known_entities
+from gemsModules.delegator.services.known_entities.server import (
+    Serve as serve_known_entities,
+)
 
 from gemsModules.logging.logger import Set_Up_Logging
+
 log = Set_Up_Logging(__name__)
 
-service_modules : Dict[str, Callable] = {
-    'Error' : serve_error,
-    'KnownEntities' : serve_known_entities,
-    'ListServices': serve_list_services, 
-    'Marco': serve_marco, 
-    'Status': serve_status
-    }
+service_modules: Dict[str, Callable] = {
+    "Error": serve_error,
+    "listEntities": serve_known_entities,  # json contract compatibility
+    "KnownEntities": serve_known_entities,
+    "ListServices": serve_list_services,
+    "Marco": serve_marco,
+    "Status": serve_status,
+}
