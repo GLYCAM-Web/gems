61743861a098986452076a94e1b45fa4dee93fdb
Grayson Miller <grayson.miller124@gmail.com>
2023-09-27 00:34:16 -0600

    added test 018 to test various unittests at once - instance ops only right now. disabled by default

---
+++ tests/018.test-disabled.gemsModules_unittests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+THISPYTHON='python3'
+testNumber=018
+echo "Testing $0..."
+
+# This test aggregates specific python unittests from the gemsModules package into a pre-push test.
+
+tests=(
+    $GEMSHOME/gemsModules/systemoperations/tests/test_instance_ops.py
+)
+
+for test in ${tests[@]}; do
+    $THISPYTHON $test
+    if [ $? -ne 0 ]; then
+        printf "%s FAILED! \n" $test
+        return 1
+    fi
+done
