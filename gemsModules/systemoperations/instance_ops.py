c7a1d41ecc18daaff677e68e3e091ef367c1f197
Grayson Miller <grayson.miller124@gmail.com>
2023-10-10 09:25:52 -0600

    get_available_contexts fix

--- gemsModules/systemoperations/instance_ops.py
+++ gemsModules/systemoperations/instance_ops.py
@@ -154,14 +154,21 @@ class InstanceConfig(dict):
     def get_available_contexts(self, instance_hostname=None) -> list:
         """
         Returns a list of available contexts for the active GEMS instance.
+
+        Can be keyed by a given name (["hosts"] key) or hostname (["hosts"][host]['host']),
+        but the instance config must contain the real hostname in either. (In some cases,
+        you need to use the ip for the host, in which case the hosts key must be the
+        hostname.)
+
         """
         if instance_hostname is None:
             instance_hostname = socket.gethostname()
 
         available_contexts = []
         for host in self["hosts"].keys():
-            if instance_hostname == host:
-                available_contexts.extend(host["contexts"])
+            h = self["hosts"][host]
+            if instance_hostname == host or instance_hostname == h["host"]:
+                available_contexts.extend(h["contexts"])
         return available_contexts
 
     def get_possible_hosts_for_context(
