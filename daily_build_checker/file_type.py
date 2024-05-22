49f375b77be8ac843bf096598e1698f8c1f4fb8a
holmess2013 <samholmes285@gmail.com>
2024-05-22 11:34:33 -0400

    file_type module
    I know have a working module called file_type.py that given a pdb file will return a message containing "PDB file" and the MIME type which is ASCII.

--- daily_build_checker/file_type.py
+++ daily_build_checker/file_type.py
@@ -1,15 +1,14 @@
 import magic
 
-def get_mime_type(file):
+def get_file_type(file):
         
-      """This function returns the MIME type of a file"""
+      """This function returns the file type and MIME
+            type of the file, by testing against custom
+            libmagic database files made for the files we 
+            generate."""
+      mag_obj = magic.Magic(magic_file = "pdb_magic")
+      return(mag_obj.from_file(file))
 
-      mime_type = magic.from_file(file, mime=True)
-
-      if mime_type:
-            return(mime_type)
-      else:
-            return("Unknown MIME Type")
 
         
         
---
+++ daily_build_checker/test_custom_magic_file.py
@@ -0,0 +1,8 @@
+import magic
+import os
+
+
+from file_type import get_file_type
+
+print(get_file_type("Galactose.pdb"))
+
