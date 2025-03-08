7c020ee738cd9b458445c2ea912c3cd8f0bf5e32
Grayson Miller <grayson.miller124@gmail.com>
2025-03-08 16:12:43 -0500

    updated json client to allow hostport selection on run - like SlurmClient

--- gRPC/JSON/json_client.py
+++ gRPC/JSON/json_client.py
@@ -1,23 +1,39 @@
-import logging
-
 import grpc
-import os
+import os, sys
 
 import json_pb2
 import json_pb2_grpc
 
+from gemsModules.logging.logger import new_concurrent_logger
+
+log = new_concurrent_logger(__name__)
+
+
 class JSONClient():
-    def __init__(self, json):
+    def __init__(self, json, host=None, port=None):
         self.json = json
-        self.response = self.run()
+        self.response = self.run(theHost=host, thePort=port)
 
     def run(self):
-
-        with grpc.insecure_channel(os.getenv('GRPC_DELEGATOR_HOST') + ':' + os.getenv('GRPC_DELEGATOR_PORT')) as channel:
-        # with grpc.insecure_channel('localhost:50051') as channel:
+        if theHost is None:
+            theHost = os.getenv('GRPC_DELEGATOR_HOST')
+            if theHost is None:
+                log.error("The gRPC/JSON server host is not defined.  Exiting.")
+                sys.exit(1)
+        if thePort is None:
+            thePort = os.getenv('GRPC_DELEGATOR_PORT')
+            if theHost is None:
+                log.error("The gRPC/JSON server port is not defined.  Exiting.")
+                sys.exit(1)
+                
+        hostport = theHost + ":" + thePort
+        log.debug("hostport is >>>" + hostport + "<<<")
+        with grpc.insecure_channel(hostport) as channel:
             stub = json_pb2_grpc.JSONStub(channel)
+            log.debug("Attempting to send %s over %s", self.json, hostport)
             response = stub.JSON_Delegator(json_pb2.JSONRequest(input=self.json))
-            #print("json_client returning a response: \n" + str(response) )
+            log.debug("json_client returns this response: \n" + str(response))
+            
         return response
 
 
