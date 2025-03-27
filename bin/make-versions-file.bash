aa03691738e2019c9710d1d29307935ade491a79
gitoliver <olgrant@uga.edu>
2025-03-27 10:08:37 +0100

    feature: gmml2 version info added to VERSIONS.sh

--- bin/make-versions-file.bash
+++ bin/make-versions-file.bash
@@ -1,29 +1,23 @@
 #!/usr/bin/env bash
-#
-# This can be called from anywhere, but the following
-#    must be accessible and writable.
-#
-#    ${GEMSHOME}
-#    ${GEMSHOME}/gmml
-# 
 echo "Getting and setting Versions info."
 
-##  Set the GEMS info
+##  Check GEMSHOME is set and is an accessible directory.
 echo "...GEMS"
 if [ "${GEMSHOME}zzz" == "zzz" ] ; then
 	echo "GEMSHOME must be set."
 	exit 1
 fi
 if ! cd ${GEMSHOME} ; then
-        echo "Could not cd to GMSHOME directory"
+        echo "Could not cd to \$GEMSHOME directory: $GEMSHOME"
 	exit 1
 fi
+## Set the GEMS version info
 echo """
 GEMS_GIT_BRANCH=\"$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')\"
 GEMS_GIT_COMMIT_HASH=\"$(git rev-parse HEAD)\"
-""" > VERSIONS.sh
+""" > $GEMSHOME/VERSIONS.sh
 
-## Set and get the GMML info
+## Set the GMML version info
 echo "...GMML"
 if ! cd "${GEMSHOME}/gmml" ; then
 	echo "Could not cd to GEMSHOME/gmml directory"
@@ -35,13 +29,20 @@ if ! ./make-versions-file.bash ; then
 	echo "Cannot complete acquisition of versions info."
 	exit 1
 fi
-if ! cd ${GEMSHOME} ; then
-        echo "Could not cd to GMSHOME directory the second time"
-        echo "How did this even happen?"
-	exit 1
+cat $GEMSHOME/gmml/VERSIONS.sh >> $GEMSHOME/VERSIONS.sh
+## Set and get the GMML2 info
+echo "...GMML2"
+if ! cd "${GEMSHOME}/gmml2" ; then
+    echo "Could not cd to GEMSHOME/gmml2 directory"
+    echo "Cannot complete acquisition of versions info."
+    exit 1
 fi
-cat gmml/VERSIONS.sh >> VERSIONS.sh
-
+if ! ./make-versions-file.bash ; then
+    echo "Could not generate gmml2 versions file"
+    echo "Cannot complete acquisition of versions info."
+    exit 1
+fi
+cat $GEMSHOME/gmml2/VERSIONS.sh >> $GEMSHOME/VERSIONS.sh
 ## Set and get the MD_Utils info
 echo "...MD_Utils"
 if ! cd "${GEMSHOME}/External/MD_Utils" ; then
@@ -54,12 +55,7 @@ if ! ./scripts/make-versions-file.bash ; then
 	echo "Cannot complete acquisition of versions info."
 	exit 1
 fi
-if ! cd ${GEMSHOME} ; then
-        echo "Could not cd to GMSHOME directory the third time"
-        echo "How did this even happen?"
-	exit 1
-fi
-cat External/MD_Utils/VERSIONS.sh >> VERSIONS.sh
+cat $GEMSHOME/External/MD_Utils/VERSIONS.sh >> $GEMSHOME/VERSIONS.sh
 
 ## Set and get the GM_Utils info
 echo "...GM_Utils"
@@ -73,13 +69,6 @@ if ! ./scripts/make-versions-file.bash ; then
 	echo "Cannot complete acquisition of versions info."
 	exit 1
 fi
-if ! cd ${GEMSHOME} ; then
-        echo "Could not cd to GMSHOME directory the fourth time"
-        echo "How did this even happen?"
-	exit 1
-fi
-cat External/GM_Utils/VERSIONS.sh >> VERSIONS.sh
+cat $GEMSHOME/External/GM_Utils/VERSIONS.sh >> $GEMSHOME/VERSIONS.sh
 
 echo "VERSIONS.sh is finished."
-
-
