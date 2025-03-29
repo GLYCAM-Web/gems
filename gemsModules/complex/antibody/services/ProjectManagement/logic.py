f92b42021a8c0d3299f0305f7ff39f7053fa70cf
Grayson Miller <grayson.miller124@gmail.com>
2025-03-29 00:00:26 -0400

    updated to configure ad2config and gwconfig for the AD2 update

--- gemsModules/complex/antibody/services/ProjectManagement/logic.py
+++ gemsModules/complex/antibody/services/ProjectManagement/logic.py
@@ -11,8 +11,7 @@ from gemsModules.systemoperations.environment_ops import get_site_version
 # from gemsModules.complex.antibody.tasks import batchcompute
 from .api import ProjectManagement_Inputs, ProjectManagement_Outputs, PM_Resource
 
-from gemsModules.complex.antibody.tasks import create_ad2cliconfig
-
+from gemsModules.complex.antibody.tasks.create_configs import create_ad2config, create_gwconfig
 from gemsModules.logging.logger import Set_Up_Logging
 
 log = Set_Up_Logging(__name__)
@@ -56,84 +55,36 @@ def execute(inputs: ProjectManagement_Inputs) -> ProjectManagement_Outputs:
             # os.symlink(source, target)
         service_outputs.resources.add_resource(file_resource)
         
-    # Run AAD2/0.configure/setup_AD_directory, then update ad2config
     # Note: AAD2 is not in the DevEnv yet, otherwise - perhaps:
     # GEMSHOME = os.environ.get("GEMSHOME")/External/AAD2
     # TODO: use GW_DOMAIN to change test/actual/dev
     site_version = "swarmtest" # get_site_version() # Note: We are currently using swarmtest explicitly for all AAD2 dev.
-    subprocess.run([f"/programs/website_aad2/{site_version}/AAD2_Docker/image/AAD2/0.configure/setup_AD_Directory"], cwd=inputs.projectDir)
-
-    # move ad2config.example to ad2config and modify it
-    shutil.move(f"{inputs.projectDir}/ad2config.example", f"{inputs.projectDir}/ad2config")
-    replacements = {
-        "Antibody_File_Name": antibody_name,
-        "Glycan_File_Name": ligand_name,
-        #"CONTAINER_NAME_PREFIX": f"{inputs.pUUID[:6]}-antibody-docking",
-        "CONTAINER_NAME_PREFIX": "",
-        "Computing_Mode": "Batch",
-        "Use_Docker": "True",
-        "AD2_Docking_Batch_Script": "submit_docking_to_slurm_with_docker.bash",
-        "AD2_Docking_Local_Script": "gwconfig"
-        # "GLYCAM_FLEXIBILITY": "Partial",
-    }
-    replace_bash_variable_in_file(f"{inputs.projectDir}/ad2config", replacements)
     
-    # remove AD2_Random_Seeds=.*EOF from ad2config
-    with open(f"{inputs.projectDir}/ad2config", "r") as f:
-        lines = f.readlines()
-    with open(f"{inputs.projectDir}/ad2config", "w") as f:
-        for line in lines:
-            if "AD2_Random_Seeds" not in line:
-                f.write(line)
-            else:
-                break
+    create_gwconfig(inputs.projectDir+"gwconfig", inputs.pUUID)
             
-    # Create ad2cliconfig (different from AD2config) Note: Not needed for AD to run
-    # create_ad2cliconfig.execute(inputs.projectDir)
-    
-    # Move ad2dockerconfig.example to ad2dockerconfig and update it approriately
-    shutil.move(f"{inputs.projectDir}/ad2dockerconfig.example", f"{inputs.projectDir}/ad2dockerconfig")
-
+    create_ad2config(inputs.projectDir+"ad2config", antibody_name, ligand_name)
+    image_name, tag_name = None, None
     with open(f"/programs/website_aad2/{site_version}/AAD2_Docker/settings.bash", "r") as f:
-        ad2dockerconfig = f.readlines()
-        # export AAD2_IMAGE_NAME="antibody-docking"
-        # export AAD2_TAG_NAME="2025-03-05-09-36-blf
-        for line in ad2dockerconfig:
+        settings = f.readlines()
+        for line in settings:
             if "AAD2_IMAGE_NAME" in line:
-                image_name = line.split("=")[1].strip().strip('"')
+                image_name = line.split("=")[1].strip().strip('"\'')
             if "AAD2_TAG_NAME" in line:
-                tag_name = line.split("=")[1].strip().strip('"')
+                tag_name = line.split("=")[1].strip().strip('"\'')
 
     replacements = {
-        "Image": f"{image_name}:{tag_name}",
         "AAD2_DOCKER_HOME": f"/programs/website_aad2/{site_version}/AAD2_Docker",
     }
-    replace_bash_variable_in_file(f"{inputs.projectDir}/ad2dockerconfig", replacements)
-    
-    # Note: Not in DevEnv for now:
-    # gwconfig from /programs/gems/External/GW_Stack_for_AAD2/gwconfig.example
-    gwconfig_example = f"/programs/website_aad2/{site_version}/GW_Stack_for_AAD2/gwconfig.example"
-    gwconfig = f"{inputs.projectDir}/gwconfig"
-    shutil.copy(gwconfig_example, gwconfig)
-    replacements = {
-        "pUUID": inputs.pUUID,
-        #"DOCKING_REPLICA_CPUS": "56",
-    }
-    replace_bash_variable_in_file(gwconfig, replacements)
-    
+    if image_name and tag_name:
+        replacements["Image"] = f"{image_name}:{tag_name}"
+    replace_bash_variable_in_file(f"{inputs.projectDir}/ad2config", replacements)
+
+        
     # slurm_submit_docking /programs/gems/External/GW_Stack_for_AAD2/submit_docking_to_slurm_with_docker.bash
-    slurm_submit_docking = f"/programs/website_aad2/{site_version}/GW_Stack_for_AAD2/submit_docking_to_slurm_with_docker.bash"
+    slurm_submit_docking = f"/programs/website_aad2/{site_version}/AAD2_Docker/image/AAD2/bin/submit_docking_to_slurm_with_docker.bash"
     actual_submit_docking = shutil.copy(slurm_submit_docking, inputs.projectDir)
-    # change #SBATCH --cpus-per-task=${DOCKING_REPLICA_CPUS}
-    # to #SBATCH --cpus-per-task=56
-    with open(actual_submit_docking, "r") as f:
-        lines = f.readlines()
-    with open(actual_submit_docking, "w") as f:
-        for line in lines:
-            if "#SBATCH --cpus-per-task=${DOCKING_REPLICA_CPUS}" in line:
-                f.write("#SBATCH --cpus-per-task=56\n")
-            else:
-                f.write(line)
+    if not os.path.exists(actual_submit_docking):
+        log.warning(f"GM/ProjectManagement: Error copying {slurm_submit_docking} to {inputs.projectDir}")
 
     
     # # TODO: we can use PM_Resource.copy_to to copy the files to the output directory.
--- gemsModules/complex/antibody/tests/test_build_remote.sh
+++ gemsModules/complex/antibody/tests/test_build_remote.sh
@@ -31,12 +31,12 @@ if ! pgrep -f "ssh -L 42099:localhost:42099 -N webdev@thoreau" > /dev/null; then
 fi
 
 echo "Now requesting the evaluation..."
-PROJECT_DIR=$(./bin/delegate gemsModules/complex/antibody/tests/inputs/explicit_evaluate-thoreau.json | tee ad2_remote_test_eval_output-gitignoreme.txt | grep -Po '"project_dir":\s*"\K[^"]*')\
+PROJECT_DIR=$(./bin/delegate gemsModules/complex/antibody/tests/inputs/explicit_evaluate-thoreau.json | tee ad2_remote_test_eval_output-git-ignore-me.txt | grep -Po '"project_dir":\s*"\K[^"]*')\
   || { echo "Failed to get project_dir from Evaluation delegation, do we have connection?"; exit 1; }
 
 PUUID=$(echo "$PROJECT_DIR" | awk -F'/' '{print $NF}')
 if [ -z "$PUUID" ]; then
-    echo "Failed to get PUUID from project_dir..."
+    echo "Failed to get pUUID from project_dir..."
     cat ad2_remote_test_eval_output-git-ignore-me.txt
     exit 1
 else
