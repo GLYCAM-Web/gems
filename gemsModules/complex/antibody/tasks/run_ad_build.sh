894ca20efbe2d47f42f27420f35c79cc635076c9
Grayson Miller <grayson.miller124@gmail.com>
2025-03-29 00:52:13 -0400

    Fixes for run ad scripts: build/evaluate

--- gemsModules/complex/antibody/tasks/run_ad_build.sh
+++ gemsModules/complex/antibody/tasks/run_ad_build.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-source ~/.bash_profile
 set -e
-
-cd ${WD}
-# export CONTAINER_NAME_PREFIX
-# export GW_STACK_PATH_PREFIX
-export WD
-# from ad2config now export AAD2_DOCKER_HOME="$GW_STACK_PATH_PREFIX/AAD2_Docker"
+source ~/.bash_profile
 source ad2config
 
-# source /programs/website_aad2/test/GW_Stack_for_AAD2/node_setup.bash
-#export GW_AAD2_STACK_HOME="$GW_STACK_PATH_PREFIX/GW_Stack_for_AAD2"
-#cd ${GW_AAD2_STACK_HOME}
-#module load iptables/1.8.7
-module load docker/24.0.7 
-#bash ensure_image_is_present.bash 
+export WD
+export PATH="${AAD2_CLI_BIN_PATH}:${PATH}"
 
 cd ${WD}
 COMMAND="bash submit_and_spawn_monitor"
--- gemsModules/complex/antibody/tasks/run_ad_evaluate.sh
+++ gemsModules/complex/antibody/tasks/run_ad_evaluate.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
+set -e
 source ~/.bash_profile
+source ad2config
+
+export WD
+export PATH="${AAD2_CLI_BIN_PATH}:${PATH}"
 
 cd ${WD}
-source ad2dockerconfig
-export CONTAINER_NAME_PREFIX
-export GW_STACK_PATH_PREFIX
-export WD 
-export AAD2_DOCKER_HOME="$GW_STACK_PATH_PREFIX/AAD2_Docker"
-export PATH="$GW_STACK_PATH_PREFIX/bin:$PATH"
-
-# source /programs/website_aad2/test/GW_Stack_for_AAD2/node_setup.bash
-export GW_AAD2_STACK_HOME="$GW_STACK_PATH_PREFIX/GW_Stack_for_AAD2"
-cd ${GW_AAD2_STACK_HOME}
-module load iptables/1.8.7
-module load docker/24.0.7 
-bash ensure_image_is_present.bash 
-
 cd ${AAD2_DOCKER_HOME}
 COMMAND="bash bin/run_aad2_command.bash ${WD} AD_Evaluate"
 eval ${COMMAND}
