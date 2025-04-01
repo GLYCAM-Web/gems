487d06c54927b4591d29778ec1b0028af38eb799
gitoliver <olgrant@uga.edu>
2025-04-01 12:43:35 +0200

    Updated to use function
    Prepping to move everything up to DEV ENV level

--- bin/generate-versions-info.bash
+++ bin/generate-versions-info.bash
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
-# OG: thought about a function to pass info into, but GMML2 is unique with a version number. This is quite clear so fine.
+
+# If a repo has a versions.txt file that it generates, like gmml2 does, you get that extra info.
+generateRepoInfo () {
+    repoPath=$1
+    repoName=$2
+    echo "...$repoName"
+    echo """
+
+${repoName}_GIT_BRANCH=\"$(git -C $repoPath rev-parse --abbrev-ref HEAD)\"
+${repoName}_GIT_COMMIT_HASH=\"$(git -C $repoPath rev-parse HEAD)\"""" >> $GEMSHOME/versions-info.txt
+    if [ -f $repoPath/version.txt ]; then
+        echo "${repoName}_VERSION=\"$(cat $repoPath/version.txt)\"" >> $GEMSHOME/versions-info.txt
+    fi
+   
+}
+
 echo "Getting and setting Versions info."
+> $GEMSHOME/versions-info.txt
 
 ##  Check GEMSHOME is set.
 if [ "${GEMSHOME}zzz" == "zzz" ] ; then
@@ -9,48 +25,12 @@ if [ "${GEMSHOME}zzz" == "zzz" ] ; then
 fi
 
 # Set version info for each repo:
-echo "...GEMS"
-echo """
-GEMS_GIT_BRANCH=\"$(git -C $GEMSHOME rev-parse --abbrev-ref HEAD)\"
-GEMS_GIT_COMMIT_HASH=\"$(git -C $GEMSHOME rev-parse HEAD)\"
-""" > $GEMSHOME/versions-info.txt
-
-echo "...GMML"
-echo """
-GMML_GIT_BRANCH=\"$(git -C $GEMSHOME/gmml rev-parse --abbrev-ref HEAD)\"
-GMML_GIT_COMMIT_HASH=\"$(git -C $GEMSHOME/gmml rev-parse HEAD)\"
-""" >> $GEMSHOME/versions-info.txt
-
-echo "...GMML2"
-echo """
-GMML2_GIT_BRANCH=\"$(git -C $GEMSHOME/gmml2 rev-parse --abbrev-ref HEAD)\"
-GMML2_GIT_COMMIT_HASH=\"$(git -C $GEMSHOME/gmml2 rev-parse HEAD)\"
-GMML2_VERSION=\"$(cat $GEMSHOME/gmml2/version.txt)\"
-""" >> $GEMSHOME/versions-info.txt
-
-echo "...MD_Utils"
-echo """
-MD_UTILS_GIT_BRANCH=\"$(git -C $GEMSHOME/External/MD_Utils rev-parse --abbrev-ref HEAD)\"
-MD_UTILS_GIT_COMMIT_HASH=\"$(git -C $GEMSHOME/External/MD_Utils rev-parse HEAD)\"
-""" >> $GEMSHOME/versions-info.txt
-
-echo "...GM_Utils"
-echo """
-GM_UTILS_GIT_BRANCH=\"$(git -C $GEMSHOME/External/GM_Utils rev-parse --abbrev-ref HEAD)\"
-GM_UTILS_GIT_COMMIT_HASH=\"$(git -C $GEMSHOME/External/GM_Utils rev-parse HEAD)\"
-""" >> $GEMSHOME/versions-info.txt
-
-echo "...AAD2"
-echo """
-AAD2_GIT_BRANCH=\"$(git -C $GEMSHOME/External/AAD2 rev-parse --abbrev-ref HEAD)\"
-AAD2_GIT_COMMIT_HASH=\"$(git -C $GEMSHOME/External/AAD2 rev-parse HEAD)\"
-""" >> $GEMSHOME/versions-info.txt
-
-echo "...GW_Stack_for_AAD2"
-echo """
-GW_Stack_for_AAD2_GIT_BRANCH=\"$(git -C $GEMSHOME/External/GW_Stack_for_AAD2 rev-parse --abbrev-ref HEAD)\"
-GW_Stack_for_AAD2_GIT_COMMIT_HASH=\"$(git -C $GEMSHOME/External/GW_Stack_for_AAD2 rev-parse HEAD)\"
-""" >> $GEMSHOME/versions-info.txt
-
+generateRepoInfo $GEMSHOME GEMS
+generateRepoInfo $GEMSHOME/gmml GMML
+generateRepoInfo $GEMSHOME/gmml2 GMML2
+generateRepoInfo $GEMSHOME/External/MD_Utils MD_Utils
+generateRepoInfo $GEMSHOME/External/GM_Utils GM_Utils
+generateRepoInfo $GEMSHOME/External/AAD2 AAD2 
+generateRepoInfo $GEMSHOME/External/GW_Stack_for_AAD2 GW_Stack_for_AAD2
 
 echo "$0 is finished. See $GEMSHOME/versions-info.txt for output."
