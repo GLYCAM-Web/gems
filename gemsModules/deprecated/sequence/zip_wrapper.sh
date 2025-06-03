0987d288dc1a15258d7c435f9e8b09d4cd4de5db
Grayson Miller <grayson.miller124@gmail.com>
2025-06-03 12:37:45 -0400

    Fix logfile names and logging dests to match MD_Utils better

--- gemsModules/deprecated/sequence/zip_wrapper.sh
+++ gemsModules/deprecated/sequence/zip_wrapper.sh
@@ -16,13 +16,13 @@ fi
 zip -ru ${project_dir_name}/${project_zipname} ${project_dir_name}/Requested_Builds ${project_dir_name}/logs -x "/*.zip"
 EXIT_CODE=$?
 if [ EXIT_CODE -ne 0 ]; then
-    echo "Failed to create the project level zip" >> ${project_dir}/zip-status.log
-    echo "[ERROR] : $(date) : Failed to zip project" >> ${project_dir}/zip-details.log
+    echo "Failed to create the project level zip" >> ${project_dir}/zip_details.log
+    echo "[ERROR] : $(date) : Failed to zip project" >> ${project_dir}/zip_status.log
 else
     # See: https://github.com/GLYCAM-Web/MD_Utils/blob/feature_add-zipfile-creation/protocols/Glycan/Prep_and_Minimization/Sequence-Prep.bash#L339
     # For info on where these patterns come from.
-    echo "Processing completed on $(date) " >> ${project_dir}/zip-status.log
-    echo "[INFO] : $(date) : Project completed" >> ${project_dir}/zip-details.log
+    echo "Processing completed on $(date) " >> ${project_dir}/zip_details.log
+    echo "[INFO] : $(date) : Project completed" >> ${project_dir}/zip_status.log
 fi
 
 exit ${EXIT_CODE}
