4dc8b2295cbedae8ffa48610aca631fa96125e51
B. Lachele Foley <lachele@gmail.com>
2026-02-11 08:23:19 -0500

    The test of instance config generation works (but not in the DevEnv containers).

--- tests/023.test-disabled.InstanceConfig-generation.sh
+++ tests/023.test-disabled.InstanceConfig-generation.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+#### READ ME
+##
+## This test cannot be a standard part of the testing suite until:
+##
+##     - The location and name of the output file 'instance_config.json' can be controlled.
+##         Currently it only overwrites the one in GEMSHOME.
+##     - The JSON files output by the IC module are always the same.
+##         The IC module does not use Pydantic, so order in the json output is not predictable.
+##         An alternative is to use the 'jq' utility. I am doing that for now, but that's a kluge.
+##         The IC module will soon use Pydantic, that change being the reason for writing this test.
+##
+####
+
 # If GEMS_KEEP_BAD_OUTPUTS is set to "True", badOutputs will not be removed after testing
 
 . './utilities/common_environment.bash'
@@ -10,12 +23,11 @@ echo "The output path is: ${GEMS_OUTPUT_PATH}"
 ## The variable badOutDir should be defined in the script that calls this one.
 outputFilePrefix='git-ignore-me_test023'
 badOutputPrefix="${badOutDir}/${now}_${outputFilePrefix}"
-badOutput="${badOutputPrefix}.txt"
 badOutputDir="${badOutDir}/${now}_${outputFilePrefix}_Files"
+correctFilesPath="correct_outputs/023.InstanceConfig-generation_Files"
 
 mkdir -p "${badOutputDir}"
 
-ALL_JSON_ARE_GOOD='true'
 ALL_TESTS_PASSED='true'
 
 echo "Testing Instance Config Generation"
@@ -23,40 +35,116 @@ echo "Testing Instance Config Generation"
 # Source the inputs for the test
 source "inputs/023.instance_config_input_swarm.bash"
 
+filesAreSame()
+{
+	#echo "working on files:"
+	#echo "        ${1}"
+	#echo "        ${2}"
+	jq -S 'walk(if type == "array" then sort else . end)' ${1} > ${badOutputDir}/sort1.json
+	jq -S 'walk(if type == "array" then sort else . end)' ${2} > ${badOutputDir}/sort2.json
+	COMMAND="diff -U 0 ${badOutputDir}/sort1.json ${badOutputDir}/sort2.json | grep -v ^@ | wc -l"
+	DiffCount="$(eval $COMMAND)"
+	result="$?"
+	#echo "DiffCount is ${DiffCount}"
+	#echo "result is ${result}"
+	if [ "${result}" != "0" ] ; then
+		rclr "Saving results diff that exited with code ${result}." "${DiffCount}" "Failed diff logging"
+		rm -f ${badOutputDir}/sort1.json ${badOutputDir}/sort2.json
+		return "${result}"
+	fi
+	if [ "${DiffCount}" != "0" ] ; then
+		COMMAND="diff ${badOutputDir}/sort1.json ${badOutputDir}/sort2.json "
+		rclr "Saving diffs from failed comparision." "${COMMAND}" "Failed comparison logging"
+		result="$?"
+		rm -f ${badOutputDir}/sort1.json ${badOutputDir}/sort2.json
+		return "$((result+1))"
+	else
+		rm -f ${badOutputDir}/sort1.json ${badOutputDir}/sort2.json
+		return 0
+	fi
+}
 
+declare -A lFailed  # generation of the local preconfig passed
+declare -A oFailed  # updating the main instance config from the local preconfig
+declare -A rFailed  # generation of the remote preconfig passed
+oFailedOverall="0"
 
-# Generate the preconfig files for the GRPC/Delegator instance and remote hosts.
-MD_PRECONFIG_NAME="MDaaS-RunMD_preconfig-git-ignore-me.json"
-MD_LOCAL_PRECONFIG_PATH="${badOutputDir}/local.${MD_PRECONFIG_NAME}"
-MD_REMOTE_PRECONFIG_PATH="${badOutputDir}/remote.${MD_PRECONFIG_NAME}"
+export STATUSFILE="${badOutputPrefix}.txt"
+export LOGFILE="${badOutputPrefix}_details.txt"
+#export TEST="True"
+#export GEMS_KEEP_BAD_OUTPUTS="True"
+passedSum="0"
+for service in ${Services[@]} ; do 
+	PreconfName="${PreconfigName[${service}]}"
+	CorrectPreconfName="${PreconfName/-git-ignore-me/}"
+	locICPath="${badOutputDir}/local.${PreconfName}"
+	remICPath="${badOutputDir}/remote.${PreconfName}"
+	IChName="${SubmissionHostNames[${service}]}"
+	IChost="${SubmissionHosts[${service}]}"
+	ICport="${SubmissionHostPorts[${service}]}"
+	ICargs="${BatchSubmissionArgs[${service}]}"
+	locParm="${LocalParameters[${service}]}"
+	locExePath="${LocalWorkingPathHead[${service}]}"
+	remExePath="${RemoteWorkingPathHead[${service}]}"
 
-GM_PRECONFIG_NAME="Glycomimetics_preconfig-git-ignore-me.json"
-GM_LOCAL_PRECONFIG_PATH="${badOutputDir}/local.${GM_PRECONFIG_NAME}"
-GM_REMOTE_PRECONFIG_PATH="${badOutputDir}/remote.${GM_PRECONFIG_NAME}"
+	## Generate the local preconfig
+        COMMAND="python3 ${GEMSHOME}/bin/setup-instance.py --generate-preconfig '${service}' '${locICPath}' '${IChName}' '${IChost}' '${ICport}' '${ICargs}' '${locParm}' '${locExePath}'"
+	rclr "Generating the local preconfig for service ${service}." "${COMMAND}" "Local IC setup"
+	lFailed[${service}]="$?"
+	if ! filesAreSame "${locICPath}"  "${correctFilesPath}/local.${CorrectPreconfName}" ; then
+		lFailed[${service}]="$((lFailed[${service}]+1))"
+	fi
+	passedSum=$((passedSum+lFailed[${service}]))
 
-AD_PRECONFIG_NAME="AntibodyDocking_preconfig-git-ignore-me.json"
-AD_LOCAL_PRECONFIG_PATH="${badOutputDir}/local.${AD_PRECONFIG_NAME}"
-AD_REMOTE_PRECONFIG_PATH="${badOutputDir}/remote.${AD_PRECONFIG_NAME}"
+	## Update the main instance config with the local information
+        COMMAND="python3 ${GEMSHOME}/bin/setup-instance.py --config '${locICPath}'"
+	rclr "Adding local info to the main instance config." "${COMMAND}" "Add local IC to main IC"
+	oFailed[${service}]="$?"
+	passedSum=$((passedSum+oFailed[${service}]))
 
-GP_PRECONFIG_NAME="GlycoProtein_preconfig-git-ignore-me.json"
-GP_LOCAL_PRECONFIG_PATH="${badOutputDir}/local.${GP_PRECONFIG_NAME}"
-GP_REMOTE_PRECONFIG_PATH="${badOutputDir}/remote.${GP_PRECONFIG_NAME}"
+	## Generate the remote preconfig
+        COMMAND="python3 ${GEMSHOME}/bin/setup-instance.py --generate-preconfig '${service}' '${remICPath}' '${IChName}' '${IChost}' '${ICport}' '${ICargs}' '${locParm}' '${remEXePath}'"
+	rclr "Generating the remote preconfig for service ${service}." "${COMMAND}" "Remote IC setup"
+	rFailed[${service}]="$?"
+	if ! filesAreSame "${remICPath}"  "${correctFilesPath}/remote.${CorrectPreconfName}" ; then
+		rFailed[${service}]="$((rFailed[${service}]+1))"
+	fi
+	passedSum=$((passedSum+rFailed[${service}]))
+done
 
+## This has to wait for the end because I didn't save the interim versions
+if ! filesAreSame "${GEMSHOME}/instance_config.json"  "${correctFilesPath}/instance_config.json" ; then
+	echo "the instance config files are not the same"
+	oFailedOverall="1"
+	passedSum="$((passedSum+oFailedOverall))"
+fi
 
-# TODO: skip this complicated cli and just write preconfig jsons instead.
-python3 "${GEMSHOME}/bin/setup-instance.py" --generate-preconfig MDaaS-RunMD "${MD_LOCAL_PRECONFIG_PATH}" "${MD_GRPC_HOSTNAME}" "${MD_GRPC_HOST}" "${MD_GRPC_PORT}" "${MD_SBATCH_ARGS}" "${MD_LOCAL_PARAMETERS}" "${MD_LOCAL_CLUSTER_PATH}"
-python3 "${GEMSHOME}/bin/setup-instance.py" --config "${MD_LOCAL_PRECONFIG_PATH}"
-python3 "${GEMSHOME}/bin/setup-instance.py" --generate-preconfig MDaaS-RunMD "${MD_REMOTE_PRECONFIG_PATH}" "${MD_GRPC_HOSTNAME}" "${MD_GRPC_HOST}" "${MD_GRPC_PORT}" "${MD_SBATCH_ARGS}" "${MD_LOCAL_PARAMETERS}" "${MD_REMOTE_CLUSTER_PATH}"
+if [ "${passedSum}" != "0" ] ; then
+	ALL_TESTS_PASSED='false'
+	for service in ${Services[@]} ; do 
+		echo "For the service ${service}:"
+		echo "        ${lFailed[${service}]} local preconfig actions failed."
+		echo "        ${oFailed[${service}]} update of main instance config actions failed."
+		echo "        ${rFailed[${service}]} remote preconfig actions failed."
+	done
+	if [ "${oFailedOverall}" != "0" ] ; then
+		echo "The final form of the instance config file failed."
+	fi
+	return 1
+fi
 
-python3 "${GEMSHOME}/bin/setup-instance.py" --generate-preconfig Glycomimetics "${GM_LOCAL_PRECONFIG_PATH}" "${GM_GRPC_HOSTNAME}" "${GM_GRPC_HOST}" "${GM_GRPC_PORT}" "${GM_SBATCH_ARGS}" "${GM_LOCAL_PARAMETERS}" "${GM_LOCAL_CLUSTER_PATH}"
-python3 "${GEMSHOME}/bin/setup-instance.py" --config "${GM_LOCAL_PRECONFIG_PATH}"
-python3 "${GEMSHOME}/bin/setup-instance.py" --generate-preconfig Glycomimetics "${GM_REMOTE_PRECONFIG_PATH}" "${GM_GRPC_HOSTNAME}" "${GM_GRPC_HOST}" "${GM_GRPC_PORT}" "${GM_SBATCH_ARGS}" "${GM_LOCAL_PARAMETERS}" "${GM_REMOTE_CLUSTER_PATH}"
+return 0
 
-python3 "${GEMSHOME}/bin/setup-instance.py" --generate-preconfig AntibodyDocking "${AD_LOCAL_PRECONFIG_PATH}" "${AD_GRPC_HOSTNAME}" "${AD_GRPC_HOST}" "${AD_GRPC_PORT}" "${AD_SBATCH_ARGS}" "${AD_LOCAL_PARAMETERS}" "${AD_LOCAL_CLUSTER_PATH}"
-python3 "${GEMSHOME}/bin/setup-instance.py" --config "${AD_LOCAL_PRECONFIG_PATH}"
-python3 "${GEMSHOME}/bin/setup-instance.py" --generate-preconfig AntibodyDocking "${AD_REMOTE_PRECONFIG_PATH}" "${AD_GRPC_HOSTNAME}" "${AD_GRPC_HOST}" "${AD_GRPC_PORT}" "${AD_SBATCH_ARGS}" "${AD_LOCAL_PARAMETERS}" "${AD_REMOTE_CLUSTER_PATH}"
-
-python3 "${GEMSHOME}/bin/setup-instance.py" --generate-preconfig GlycoProtein "${GP_LOCAL_PRECONFIG_PATH}" "${GP_GRPC_HOSTNAME}" "${GP_GRPC_HOST}" "${GP_GRPC_PORT}" "${GP_SBATCH_ARGS}" "${GP_LOCAL_PARAMETERS}" "${GP_LOCAL_CLUSTER_PATH}"
-python3 "${GEMSHOME}/bin/setup-instance.py" --config "${GP_LOCAL_PRECONFIG_PATH}"
-python3 "$GEMSHOMEGEMS_BASE_PATH}/bin/setup-instance.py" --generate-preconfig GlycoProtein "${GP_REMOTE_PRECONFIG_PATH}" "${GP_GRPC_HOSTNAME}" "${GP_GRPC_HOST}" "${GP_GRPC_PORT}" "${GP_SBATCH_ARGS}" "${GP_LOCAL_PARAMETERS}" "${GP_REMOTE_CLUSTER_PATH}"
+###
+## The following are records of sample commands that were run in the file I based these tests on. 
+##
+### Generate the preconfig files for the GRPC/Delegator instance and remote hosts.
+##MD_PRECONFIG_NAME="MDaaS-RunMD_preconfig-git-ignore-me.json"
+##MD_LOCAL_PRECONFIG_PATH="${badOutputDir}/local.${MD_PRECONFIG_NAME}"
+##MD_REMOTE_PRECONFIG_PATH="${badOutputDir}/remote.${MD_PRECONFIG_NAME}"
+##
+### TODO: skip this complicated cli and just write preconfig jsons instead.
+##python3 "${GEMSHOME}/bin/setup-instance.py" --generate-preconfig MDaaS-RunMD "${MD_LOCAL_PRECONFIG_PATH}" "${MD_GRPC_HOSTNAME}" "${MD_GRPC_HOST}" "${MD_GRPC_PORT}" "${MD_SBATCH_ARGS}" "${MD_LOCAL_PARAMETERS}" "${MD_LOCAL_CLUSTER_PATH}"
+##python3 "${GEMSHOME}/bin/setup-instance.py" --config "${MD_LOCAL_PRECONFIG_PATH}"
+##python3 "${GEMSHOME}/bin/setup-instance.py" --generate-preconfig MDaaS-RunMD "${MD_REMOTE_PRECONFIG_PATH}" "${MD_GRPC_HOSTNAME}" "${MD_GRPC_HOST}" "${MD_GRPC_PORT}" "${MD_SBATCH_ARGS}" "${MD_LOCAL_PARAMETERS}" "${MD_REMOTE_CLUSTER_PATH}"
 
--- tests/inputs/023.instance_config_input_swarm.bash
+++ tests/inputs/023.instance_config_input_swarm.bash
@@ -16,21 +16,23 @@ Services=(
 	"GP" 
 	) # One day might need CB, GF and others
 
-declare -A PreconfigName # This is for use in the test
+declare -A PreconfigName # This is for use in the test rather than being part of the test
 PreconfigName=(
-	["MD"]=
-	["GM"]=
-	["AD"]=
-	["GP"]=
+	["MD"]="MDaaS-RunMD_preconfig-git-ignore-me.json"
+	["GM"]="Glycomimetics_preconfig-git-ignore-me.json"
+	["AD"]="AntibodyDocking_preconfig-git-ignore-me.json"
+	["GP"]="GlycoProtein_preconfig-git-ignore-me.json"
 	)
 
-# For now, I am making these names be more descriptive, but will not change the names used in GEMS
+## These arrays correspond to parts of the instance config
+# For now, I am making these names be descriptive here in this test, but will not (yet) change the names used in GEMS
 declare -A SubmissionHosts       # Connection info (e.g., IP Addy) for hosts to which batch jobs are submitted (e.g., Slurm Hosts)
 declare -A SubmissionHostNames   # Names for these hosts - are descriptive and need not correspond to anything, but probably should for sanity
 declare -A SubmissionHostPorts   # Ports for these hosts 
 declare -A RedirectionHosts      # Same as above, but for hosts to which JSON requests should be forwarded
-declare -A RedirectionHostNames
-declare -A RedirectionHostPorts
+declare -A RedirectionHostNames  # ""
+declare -A RedirectionHostPorts  # ""
+                                 # This would be an entry in RedirectionHostPorts:  AD_JSON_PORT=42031 # UNUSED currently, but needs to be set for AD.
 declare -A BatchSubmissionArgs   # Submission arguments for the service (each service will treat as default, enforced, etc., as needed)
 declare -A LocalParameters       # If execution is local, provide some preferences
 declare -A LocalWorkingPathHead  # The parent directory for working directories on the local machine
@@ -39,6 +41,7 @@ declare -A RemoteWorkingPathHead # The parent directory for working directories 
 				 #        This is of use to sysadmins for mounting remote directories, but is not of use to GEMS, generally.
 				 #        One exception would be the preparation of instructions to be executed remotely, but it seems that
 				 #        variables could be used that are set at the remote location.
+				 # I think this is mainly used for generating mounts, but that information does not belong in GEMS.
 
 SubmissionHosts=(
 	["MD"]='172.16.4.2'
@@ -58,7 +61,6 @@ SubmissionHostPorts=(
 	["AD"]='42030'
 	["GP"]='42030'
 	)
-    AD_JSON_PORT=42031 # UNUSED currently, but needs to be set for AD.
 RedirectionHosts=(
 	["MD"]=''
 	["GM"]=''
@@ -77,7 +79,7 @@ RedirectionHostPorts=(
 	["AD"]='42030'
 	["GP"]=''
 	)
-SbatchArgs=(
+BatchSubmissionArgs=(
 	["MD"]='{ "partition": "mdaas", "time": "120", "nodes": "1", "gres": "gpu:1", "tasks-per-node": "4", "cpus-per-task": "7" }'
 	["GM"]='{ "partition": "gm", "time": "120", "nodes": "1", "gres": "gpu:1", "tasks-per-node": "4", "cpus-per-task": "7" }'
 	["AD"]='{ "partition": "amber", "time": "120", "nodes": "1", "tasks-per-node": "4" }'
